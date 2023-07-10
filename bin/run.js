9a0ded1b900cd97ab6683b59a68fb1134f4e7a83
Tomek Wytrębowicz <tomalecpub@gmail.com>
2023-07-10 18:33:35 +0200

    Start linting js files, remove non-needed comment
    addresses https://github.com/woocommerce/dewped/pull/5#discussion_r1255607077

--- bin/run.js
+++ bin/run.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// eslint-disable-next-line node/shebang
 (async () => {
   const oclif = await import('@oclif/core')
   await oclif.execute({type: 'esm', dir: import.meta.url})
